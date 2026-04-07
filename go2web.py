b8a71be706e572fa430fe8648656b67dbf6bd078
arturtugui <146761295+arturtugui@users.noreply.github.com>
2026-04-07 23:43:12 +0300

    feat: parse info from header

---
+++ go2web.py
@@ -0,0 +1,50 @@
+from socket import *
+
+serverName = 'google.com'
+serverPort = 80
+
+# Create a IPv4, TCP socket
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+# Establish a connection to the server
+clientSocket.connect((serverName, serverPort))
+
+# Send an HTTP GET request to the server
+http_request = '''GET / HTTP/1.1\r\n
+            Host: google.com\r\n
+            \r\n'''
+
+clientSocket.sendall(http_request.encode())
+
+# Receive the HTTP response from the server
+http_response_binary = b""
+while True:
+    chunk = clientSocket.recv(4096)
+    if not chunk:
+        break
+    http_response_binary += chunk
+
+http_response = http_response_binary.decode(errors="replace")
+
+# Extract response information
+header_end = http_response.find("\r\n\r\n")
+http_response_header = http_response[:header_end]
+
+status_line = http_response_header.splitlines()[0]
+protocol = status_line.split()[0]
+status_code = status_line.split()[1]
+status_message = " ".join(status_line.split()[2:])
+
+#   Split header before and after "Date: " and pick the part after
+#   Split the remaining string at the first occurrence of "\r\n" to get the date value
+date = http_response_header.split("Date: ")[1].split("\r\n")[0]
+
+expiration = http_response_header.split("Expires: ")[1].split("\r\n")[0]
+
+print(f"Protocol: {protocol}")
+print(f"Status Code: {status_code}")
+print(f"Status Message: {status_message}")
+print(f"Date: {date}")
+print(f"Expiration: {expiration}")
+
+clientSocket.close()
