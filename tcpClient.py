01bad2c81fa7e968988240adfbbc46f5a3c04c53
arturtugui <146761295+arturtugui@users.noreply.github.com>
2026-04-07 23:27:06 +0300

    refactor: make tcpClient communicate with google.com

--- tcpClient.py
+++ tcpClient.py
@@ -1,16 +1,30 @@
 from socket import *
 
-serverName = 'localhost'
-serverPort = 12000
+serverName = 'google.com'
+serverPort = 80
 
 # Create a IPv4, TCP socket
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
 clientSocket.connect((serverName, serverPort))
 
-sentence = input('Input a lowercase sentence: ')
-clientSocket.send(sentence.encode())
+http_request = '''GET / HTTP/1.1\r\n
+            Host: google.com\r\n
+            \r\n'''
 
-modifiedSentence = clientSocket.recv(1024)
-print('From Server uppercase sentence: ', modifiedSentence.decode())
+clientSocket.sendall(http_request.encode())
+
+http_response_binary = b""
+while True:
+    chunk = clientSocket.recv(4096)
+    if not chunk:
+        break
+    http_response_binary += chunk
+
+http_response = http_response_binary.decode(errors="replace")
+
+header_end = http_response.find("\r\n\r\n")
+http_response_header = http_response[:header_end]
+print(http_response_header)
+
 clientSocket.close()
