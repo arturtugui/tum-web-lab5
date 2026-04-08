73d5951af480717d3fcc812fe4a036049413b2a2
arturtugui <146761295+arturtugui@users.noreply.github.com>
2026-04-08 11:21:23 +0300

    feat: implement commands -h and -u

--- go2web.py
+++ go2web.py
@@ -1,72 +1,92 @@
 from socket import *
 import sys
 
-# parse command line arguments
-if len(sys.argv) <= 1:
-    print("Error: wrong format.")
-    sys.exit(1)
+search_engine = "Google"
 
-if len(sys.argv) == 2 and sys.argv[1] == "-h":
-    print("Help command.")
-    sys.exit(0)
+def parse_args():
+    # parse command line arguments
+    if len(sys.argv) <= 1:
+        print("Error: wrong format.")
+        sys.exit(1)
 
-if len(sys.argv) == 3 and sys.argv[1] == "-u":
-    print("URL command.")
-    host = sys.argv[2]
-    print(f"Host: {host}")
-    sys.exit(0)
+    elif len(sys.argv) == 2 and sys.argv[1] == "-h":
+        show_help()
+        sys.exit(0)
 
-if len(sys.argv) >= 3 and sys.argv[1] == "-s":
-    print("Search command.")
-    print("Search terms: " + " ".join(sys.argv[2:]))
-    sys.exit(0)
+    elif len(sys.argv) == 3 and sys.argv[1] == "-u":
+        host = sys.argv[2]
+        port = 80
+        make_http_request(host, port)
+        sys.exit(0)
 
+    elif len(sys.argv) >= 3 and sys.argv[1] == "-s":
+        print("Search command.")
+        print("Search terms: " + " ".join(sys.argv[2:]))
+        sys.exit(0)
 
-serverName = "google.com"
-serverPort = 80
+    else:
+        print("Error: wrong format.")
+        sys.exit(1)
 
-# Create a IPv4, TCP socket
-clientSocket = socket(AF_INET, SOCK_STREAM)
+def show_help():
+    print("Usage: go2web [options]")
+    print("Options:")
+    print("  -h                Show this help message and exit")
+    print("  -u <URL>         Make an HTTP request to the specified URL and print the response")
+    print(f"  -s <search terms> Make an HTTP request to search the term using {search_engine} and print top 10 results")
 
-# Establish a connection to the server
-clientSocket.connect((serverName, serverPort))
+def make_http_request(host, port):
+    # Create a IPv4, TCP socket
+    clientSocket = socket(AF_INET, SOCK_STREAM)
 
-# Send an HTTP GET request to the server
-http_request = '''GET / HTTP/1.1\r\n
-            Host: google.com\r\n
-            \r\n'''
+    # Establish a connection to the server
+    clientSocket.connect((host, port))
 
-clientSocket.sendall(http_request.encode())
+    # Send an HTTP GET request to the server
+    http_request = '''GET / HTTP/1.1\r\n
+                Host: ''' + host + '''\r\n
+                \r\n'''
 
-# Receive the HTTP response from the server
-http_response_binary = b""
-while True:
-    chunk = clientSocket.recv(4096)
-    if not chunk:
-        break
-    http_response_binary += chunk
+    clientSocket.sendall(http_request.encode())
 
-http_response = http_response_binary.decode(errors="replace")
+    # Receive the HTTP response from the server
+    http_response_binary = b""
+    while True:
+        chunk = clientSocket.recv(4096)
+        if not chunk:
+            break
+        http_response_binary += chunk
 
-# Extract response information
-header_end = http_response.find("\r\n\r\n")
-http_response_header = http_response[:header_end]
+    http_response = http_response_binary.decode(errors="replace")
 
-status_line = http_response_header.splitlines()[0]
-protocol = status_line.split()[0]
-status_code = status_line.split()[1]
-status_message = " ".join(status_line.split()[2:])
+    # Extract response information
+    header_end = http_response.find("\r\n\r\n")
+    http_response_header = http_response[:header_end]
 
-#   Split header before and after "Date: " and pick the part after
-#   Split the remaining string at the first occurrence of "\r\n" to get the date value
-date = http_response_header.split("Date: ")[1].split("\r\n")[0]
+    status_line = http_response_header.splitlines()[0]
+    protocol = status_line.split()[0]
+    status_code = status_line.split()[1]
+    status_message = " ".join(status_line.split()[2:])
 
-expiration = http_response_header.split("Expires: ")[1].split("\r\n")[0]
+    #   Split header before and after "Date: " and pick the part after
+    #   Split the remaining string at the first occurrence of "\r\n" to get the date value
+    date = http_response_header.split("Date: ")[1].split("\r\n")[0]
 
-print(f"Protocol: {protocol}")
-print(f"Status Code: {status_code}")
-print(f"Status Message: {status_message}")
-print(f"Date: {date}")
-print(f"Expiration: {expiration}")
+    expiration = http_response_header.split("Expires: ")[1].split("\r\n")[0]
 
-clientSocket.close()
+    print("\tHTTP Response Header Information:")
+    print(f"Protocol: {protocol}")
+    print(f"Status Code: {status_code}")
+    print(f"Status Message: {status_message}")
+    print(f"Date: {date}")
+    print(f"Expiration: {expiration}\n")
+
+    clientSocket.close()
+
+def main():
+    parse_args()
+
+    
+
+if __name__ == "__main__":
+    main()
